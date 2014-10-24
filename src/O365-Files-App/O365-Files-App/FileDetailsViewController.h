1fd2f15c6635266defa7aeed7e906abc3ee956f5
Lucas Damián Nápoli <lnapoli87@gmail.com>
2014-10-24 12:06:46 -0300

    added view controllers for new file and file details

---
+++ src/O365-Files-App/O365-Files-App/FileDetailsViewController.h
@@ -0,0 +1,13 @@
+//
+//  FileDetailsViewController.h
+//  O365-Files-App
+//
+//  Created by Lucas Damian Napoli on 24/10/14.
+//  Copyright (c) 2014 MS Open Tech. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface FileDetailsViewController : ViewController
+
+@end
